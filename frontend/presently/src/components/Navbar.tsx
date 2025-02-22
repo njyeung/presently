264760b81bdfcb438b6fa2c706bac34280c07436
Nick Yeung <nickjyeung@Nicks-MacBook-Air.local>
2025-02-22 13:20:54 -0600

    changes to hero

--- frontend/presently/src/components/Navbar.tsx
+++ frontend/presently/src/components/Navbar.tsx
@@ -2,8 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="w-full h-12 bg-gray-900 text-white px-6 flex justify-between items-center fixed top-0 left-0 right-0 shadow-md z-50">
-            {/* Left Side: About & Search */}
+        <nav className="w-full h-12 bg-black text-white px-6 flex justify-between items-center fixed top-0 left-0 right-0 shadow-md z-50">
             <div className="flex space-x-6">
                 <Link href="/about" className="hover:text-gray-400">
                     About
@@ -12,8 +11,6 @@ export default function Navbar() {
                     Search
                 </Link>
             </div>
-
-            {/* Center: Presently (Bold) */}
             <div className="absolute left-1/2 transform -translate-x-1/2">
                 <Link href="/" className="text-3xl font-bold">
                     Presently
--- frontend/presently/src/components/hero.tsx
+++ frontend/presently/src/components/hero.tsx
@@ -3,10 +3,10 @@ import Image from "next/image"
 import { buttonVariants } from "@/components/ui/button"
 
 export default function Hero() {
-    return <section>
-        <div className="container">
+    return <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] ">
+        <div className="container pt-10 px-4">
             <div className="md:flex items-center">
-                <div>
+                <div className="">
                     <h1 className="text-5xl md:text-7xl font-bold black bg-clip-text mt-6">
                         Presently
                     </h1>
@@ -17,7 +17,7 @@ export default function Hero() {
                         Find your perfect gift
                     </Link>
                 </div>
-                <div>
+                <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
                     {/* <Image src="https://picsum.photos/id/237/200/300" alt="picture" height={200} width={200}></Image> */}
                 </div>
             </div>
