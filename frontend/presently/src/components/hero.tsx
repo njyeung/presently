6ffa7d53e6962caee4026eeee32af78a07bc5aa5
Nick Yeung <nickjyeung@Nicks-MacBook-Air.local>
2025-02-22 14:13:44 -0600

    hero

--- frontend/presently/src/components/hero.tsx
+++ frontend/presently/src/components/hero.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link"
 import Image from "next/image"
 import { twMerge } from "tailwind-merge"
-import ArrowRight from "@/assets/ArrowRight.svg"
-
+import ArrowRight from "@/assets/arrow-right";
+import PresentBox from "@/assets/PresentBox.png"
 import { buttonVariants } from "@/components/ui/button"
+import { motion } from "framer-motion";
 
 export default function Hero() {
     return <section className="pt-10 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] ">
         <div className="contain">
             <div className="md:flex items-center">
-                <div className="max-w-[700px]">
+                <div className="max-w-[500px]">
                     <h1 className="text-4xl md:text-7xl font-bold black bg-clip-text mt-6">
                         Presently Perfect for Every Occasion
                     </h1>
@@ -22,8 +23,21 @@ export default function Hero() {
                         <ArrowRight></ArrowRight>
                     </Link>
                 </div>
-                <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-                    {/* <Image src="https://picsum.photos/id/237/200/300" alt="picture" height={200} width={200}></Image> */}
+                <div className="mt-20 relative">
+                    <motion.img 
+                        src={PresentBox.src} 
+                        alt="picture" 
+                        height={500} width={500} 
+                        animate={{
+                            translateY: [-30, 30],
+                          }}
+                          transition={{
+                            repeat: Infinity,
+                            repeatType: "mirror",
+                            duration: 3,
+                            ease: "easeInOut",
+                          }}
+                    />
                 </div>
             </div>
         </div>
