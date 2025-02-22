285b76e24a2b09b7d5ce86c82016f4154eb45af2
Nick Yeung <nickjyeung@Nicks-MacBook-Air.local>
2025-02-22 13:04:18 -0600

    changes to hero.tsx

--- frontend/presently/src/app/page.tsx
+++ frontend/presently/src/app/page.tsx
@@ -5,6 +5,5 @@ import Image from "next/image";
 export default function Home() {
   return <>
     <Hero />
-    <Hero />
   </>
 }
--- frontend/presently/src/components/hero.tsx
+++ frontend/presently/src/components/hero.tsx
@@ -1,17 +1,27 @@
 import Link from "next/link"
 import Image from "next/image"
+import { buttonVariants } from "@/components/ui/button"
 
 export default function Hero() {
     return <section>
-        <div>
-        <h1>Welcome to Giftopia!</h1>
-        <p>Your ultimate destination for personalized gift recommendations.</p>
-        <Link href="#"> 
-        {/* TODO: link to ocassion component */}
-          Find Your Perfect Gift
-        </Link>
-      </div>
-      <div >
-      </div>
+        <div className="container">
+            <div className="md:flex items-center">
+                <div>
+                    <h1 className="text-5xl md:text-7xl font-bold black bg-clip-text mt-6">
+                        Presently
+                    </h1>
+                    <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+                        Your ultimate destination for personalized gift recommendations.
+                    </p>
+                    <Link href="#" className={buttonVariants({ variant: "outline" })}>
+                        Find your perfect gift
+                    </Link>
+                </div>
+                <div>
+                    {/* <Image src="https://picsum.photos/id/237/200/300" alt="picture" height={200} width={200}></Image> */}
+                </div>
+            </div>
+        </div>
+        
     </section>
 }
