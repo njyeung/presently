d165269ef87f28ed705ea34134b74549361e7995
Nick Yeung <nickjyeung@Nicks-MacBook-Air.local>
2025-02-22 14:30:08 -0600

    framer motion doesn't work, so we're not using it lol

--- frontend/presently/src/components/hero.tsx
+++ frontend/presently/src/components/hero.tsx
@@ -1,16 +1,18 @@
+"use-client"
+
 import Link from "next/link"
 import Image from "next/image"
 import { twMerge } from "tailwind-merge"
 import ArrowRight from "@/assets/arrow-right";
 import PresentBox from "@/assets/PresentBox.png"
 import { buttonVariants } from "@/components/ui/button"
-import { motion } from "framer-motion";
 
 export default function Hero() {
+
     return <section className="pt-10 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] ">
         <div className="contain">
             <div className="md:flex items-center">
-                <div className="max-w-[500px]">
+                <div className="max-w-[500px] gap-5">
                     <h1 className="text-4xl md:text-7xl font-bold black bg-clip-text mt-6">
                         Presently Perfect for Every Occasion
                     </h1>
@@ -24,20 +26,7 @@ export default function Hero() {
                     </Link>
                 </div>
                 <div className="mt-20 relative">
-                    <motion.img 
-                        src={PresentBox.src} 
-                        alt="picture" 
-                        height={500} width={500} 
-                        animate={{
-                            translateY: [-30, 30],
-                          }}
-                          transition={{
-                            repeat: Infinity,
-                            repeatType: "mirror",
-                            duration: 3,
-                            ease: "easeInOut",
-                          }}
-                    />
+                    <Image src={PresentBox} alt="present box" width={500}/>
                 </div>
             </div>
         </div>
